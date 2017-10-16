c91258574e82ff7f8d0084a3505f59d4c8171149
杨忠林 <zy2708@email.vccs.edu>
2017-10-16 11:14:09 -0400

    first commit

---
+++ Groupsize.java
@@ -0,0 +1,109 @@
+mport javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextInputDialog;
+
+public class Groupsize extends Application
+{
+    @Override
+    public void start(Stage Primarystage)
+    {   boolean login=false;
+        int n=0;
+        do {
+            Dialog<Pair<String, String>> dialog = new Dialog<>();
+            dialog.setTitle("Login");
+            dialog.setHeaderText("Login");
+
+            ButtonType loginButtonType = new ButtonType("Login", ButtonData.OK_DONE);
+            dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+            GridPane grid = new GridPane();
+            grid.setHgap(10);
+            grid.setVgap(10);
+            grid.setPadding(new Insets(20, 150, 10, 10));
+
+            TextField username = new TextField();
+            username.setPromptText("Username");
+            PasswordField password = new PasswordField();
+            password.setPromptText("Password");
+
+            grid.add(new Label("Username:"), 0, 0);
+            grid.add(username, 1, 0);
+            grid.add(new Label("Password:"), 0, 1);
+            grid.add(password, 1, 1);
+
+            dialog.getDialogPane().setContent(grid);
+
+            Optional<Pair<String, String>> result = dialog.showAndWait();
+
+            if(username.getText().equals("csc200")&&password.getText().equals("ct0103")){
+                TextInputDialog textInputDialog = new TextInputDialog("Number of people");
+                Optional<String> num = textInputDialog.showAndWait();
+                String number = num.get();
+                double peoplenumber = Double.parseDouble(number);
+                if (peoplenumber >= 10) {
+                    double groupsize = peoplenumber / 2;
+                    Alert alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Groupsize");
+                    alert.setHeaderText("Groupsize");
+                    alert.setContentText("The number of people is " + peoplenumber + " ." + "Groupsize is " + groupsize + " .");
+                    alert.show();
+                } else if (3 <= peoplenumber) {
+                    double groupsize = peoplenumber / 3;
+                    Alert alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Groupsize");
+                    alert.setHeaderText("Groupsize");
+                    alert.setContentText("The number of people is " + peoplenumber + " ." + "Groupsize is " + groupsize + " .");
+                    alert.show();
+                } else if (peoplenumber < 3) {
+                    Alert alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Groupsize");
+                    alert.setHeaderText("Groupsize");
+                    alert.setContentText("The number of people has to be at least 3.");
+                    alert.show();
+                }
+                login=true;
+                n=0;
+            }
+            else if(!username.getText().equals("csc200")){
+                Alert alert =new Alert (AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText("Your username or password is wrong.");
+                alert.setContentText("Please enter your username and password again.");
+                alert.show();
+            }
+            else if(username.getText().equals("csc200")&&!password.getText().equals("ct0103")&&n<2){
+                Alert alert =new Alert (AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText("Your username or password is wrong.");
+                alert.setContentText("Please enter your username and password again.");
+                alert.show();
+                n++;
+            }
+            else if (username.getText().equals("csc200")&&!password.getText().equals("ct0103")&&n==2){
+                Alert alert =new Alert (AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText("You have entered wrong password three times.");
+                alert.setContentText("Please contact admistrator.");
+                alert.show();
+                n++;
+            }
+        }while(!login&&n<=2);
+
+    }
+    public static void main(String [] args) {
+        launch(args);
+    }
+}
+
